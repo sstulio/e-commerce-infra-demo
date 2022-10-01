31c4ee0bd72d58177bde13b4358d7c96d8b38a84
Túlio de Souza <tulio.souza@zimpler.com>
2022-10-01 20:48:44 +0200

    Split vpc in a different file

--- shared/gke-cluster/main.tf
+++ shared/gke-cluster/main.tf
@@ -9,31 +9,6 @@ locals {
   service_account = "terraform@zcelero-tech-talk.iam.gserviceaccount.com"
 }
 
-module "gcp-network" {
-  source       = "terraform-google-modules/network/google"
-  project_id   = var.project_id
-  network_name = "${var.network}-${var.env_name}"
-  subnets = [
-    {
-      subnet_name   = "${var.subnetwork}-${var.env_name}"
-      subnet_ip     = "10.10.0.0/16"
-      subnet_region = var.region
-    },
-  ]
-  secondary_ranges = {
-    "${var.subnetwork}-${var.env_name}" = [
-      {
-        range_name    = var.ip_range_pods_name
-        ip_cidr_range = "10.20.0.0/16"
-      },
-      {
-        range_name    = var.ip_range_services_name
-        ip_cidr_range = "10.30.0.0/16"
-      },
-    ]
-  }
-}
-
 module "gke" {
   source                     = "terraform-google-modules/kubernetes-engine/google"
   project_id                 = var.project_id
