b4b8a847055c5038f05b49cf8f5c8537318f1c3f
Túlio de Souza <tulio.souza@zimpler.com>
2022-10-13 23:32:25 +0200

    fix cert manager cluster-issuer

--- shared/cert-manager/main.tf
+++ shared/cert-manager/main.tf
@@ -5,6 +5,20 @@ terraform {
   }
 }
 
+data "kubectl_file_documents" "clusterissuer_file" {
+  content = file("cluster-issuer.yaml")
+}
+
+resource "kubectl_manifest" "clusterissuer_letsencrypt_production" {
+  count              = length(data.kubectl_file_documents.clusterissuer_file.documents)
+  yaml_body          = element(data.kubectl_file_documents.clusterissuer_file.documents, count.index)
+  override_namespace = "cert-manager"
+
+  depends_on = [
+    module.cert_manager
+  ]
+}
+
 module "cert_manager" {
   source         = "terraform-iaac/cert-manager/kubernetes"
   namespace_name = "cert-manager"
